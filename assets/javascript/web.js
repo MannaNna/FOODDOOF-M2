580c3727659dc76d58af90999c38fed7df7570ed
Mariana Secu <secu.mariana92@gmail.com>
2019-07-18 11:56:28 -0700

    Added a function for the button click toget the text from the search box

--- assets/javascript/web.js
+++ assets/javascript/web.js
@@ -1,14 +1,28 @@
-var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=&location=seattle";
-debugger;
+
+
+function searchClick(){
+    var searchStr = null;
+    searchStr = $('#txtSearch').val();
+    callYelpAPI(searchStr);
+    console.log('Search Clicked');
+  };
+
+ 
+
+function callYelpAPI(searchStr){
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=&location=" + searchStr;
+//var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=&location=seattle";
+
  $.ajax({
     url: myurl,
     headers: {
      'Authorization':'Bearer 4V6LSZr3MVNCXa9HoTVxqs-mj8y3URlF57Vio7wTF7g8aimwmPk_cM8faiKivtC9UOog9WZnv7ZFKqpy8j2xvQbb2ihQXoOBOx44GmwUmSWLdETmXrVRSFkNlCQuXXYx',
+     'Access-Control-Allow-Origin' : '*'
+     
  },
     method: 'GET',
     dataType: 'json',
     success: function(data){
-    debugger;
         // Grab the results from the API JSON return
         var totalresults = data.total;
         // If our results are greater than 0, continue
@@ -30,7 +44,11 @@ debugger;
                 var state = item.location.state;
                 var zipcode = item.location.zip_code;
                 // Append our result into our page
-                $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+                // $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+
+                var resultContainer ='<div class=\"col-md-4 results-box wow fadeInUp\"><div class="row"><div class="col-md-4"><img src="' + image + '" style="width:200px;height:150px;"><br></div><div class="col-md-8"><h3>' + name + '</h3><p><br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div></p></div></div></div>';
+
+                $('#resultsContainer').append(resultContainer);
           });
         } else {
             // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
@@ -38,7 +56,7 @@ debugger;
         }
     },
      error: function(XMLHttpRequest, textStatus, errorThrown) {
-     debugger;
-         alert("some error");
-      }	
- });      
+         alert("API ERROR");
+    }	
+ });
+}      
